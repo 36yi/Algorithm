799cb181ad0071ac501a445876125e8aa7433b06
36yi <36yunjae@gmail.com>
2024-11-05 10:46:38 +0900

    20241105

--- Topcoder/review.py
+++ Topcoder/review.py
@@ -1,27 +1,24 @@
-class Solution:
-    mi_n = 10 ** 5
+#Kiwi
+# capacities = list(map(int,input().split()))
+# bottles = list(map(int,input().split()))
+# fromId = list(map(int,input().split()))
+# toId = list(map(int,input().split()))
+#
+# returns = []
+#
+# for i in range(len(fromId)):
+#     if bottles[fromId[i]] == 0:
+#         continue
+#     else:
+#         if bottles[toId[i]] == capacities[toId[i]]:
+#             continue
+#
+#         else:
+#             s = bottles[fromId[i]] + bottles[toId[i]]
+#             bottles[toId[i]] = min(capacities[toId[i]], bottles[toId[i]] + bottles[fromId[i]])
+#             bottles[fromId[i]] = s - bottles[toId[i]]
+#
+# print(*bottles)
 
-    def minSubArrayLen(self, target: int, nums: list[int]) -> int:
-        s = sum(nums)
+#Party
 
-        for i in range(len(nums)):
-            num = s
-            for j in range(len(nums), i, -1):
-                if (j == len(nums)):
-                    pass
-                else:
-                    num -= nums[j]
-                if (num >= target):
-                    self.mi_n = min(j - i, self.mi_n)
-            s -= nums[i]
-
-        if (self.mi_n == 10 ** 5):
-            return 0
-        else:
-            return self.mi_n
-
-
-c = Solution()
-t = int(input())
-n = list(map(int,input().split()))
-print(c.minSubArrayLen(t,n))
