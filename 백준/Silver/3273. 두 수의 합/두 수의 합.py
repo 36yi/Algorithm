41e71c7979c8a31197af1a440e4c24ce36a8f3c1
36yi <97164187+36yi@users.noreply.github.com>
2024-11-07 15:19:14 +0900

    [Silver III] Title: 두 수의 합, Time: 84 ms, Memory: 45756 KB -BaekjoonHub

---
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.py
@@ -0,0 +1,14 @@
+cnt = 0
+n = int(input())
+nums = list(map(int,input().split()))
+x = int(input())
+
+li = [0] * 1000001
+for num in nums:
+    li[num] = 1
+
+for num in nums:
+    if x-num<=1000000 and li[x-num]:
+        cnt += 1
+
+print(cnt//2)
