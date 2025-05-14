cbfb49c7a42b0b37e6ec2253fa0ba050fde8a127
36yi <97164187+36yi@users.noreply.github.com>
2025-05-14 23:40:37 +0900

    [Bronze II] Title: Hashing, Time: 36 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -2,6 +2,6 @@ n = int(input())
 s = input()
 ans = 0
 for i in range(n):
-    ans += ((ord(s[i]) - ord('a') + 1) * (31 ** i)) % 1234567891
+    ans += ((ord(s[i]) - ord('a') + 1) * (31 ** i)) 
 
-print(ans)
+print(ans% 1234567891)
