d583ab8cb45a21750c3a6d14d1f0719c7b42a121
Kwonyunjae <97164187+36yi@users.noreply.github.com>
2025-09-23 15:22:10 +0900

    [Gold III] Title: 파티, Time: 48 ms, Memory: 36532 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -4,13 +4,15 @@ input = sys.stdin.readline
 
 N, M, X = map(int, input().split()) # X : target
 graph = [[] for _ in range(N+1)]
+reverse_graph = [[] for _ in range(N+1)]
 MAX = float('inf')
 
 for _ in range(M):
     a, b, c = map(int, input().split())
     graph[a].append((b, c))
+    reverse_graph[b].append((a,c))
 
-def dijkstra(start):
+def dijkstra(start,g):
     heap = []
     distance = [MAX] * (N+1)
     distance[start] = 0
@@ -21,21 +23,17 @@ def dijkstra(start):
         if cost > distance[node]:
             continue
 
-        for next_node, weight in graph[node]:
+        for next_node, weight in g[node]:
             if distance[next_node] > cost + weight:
                 distance[next_node] = cost + weight
                 heapq.heappush(heap, (distance[next_node], next_node))
     return distance
 
-target_to_home = dijkstra(X)
+target_to_home = dijkstra(X,graph)
 max_time = 0
+
+home_to_target = dijkstra(X,reverse_graph)
 for i in range(1,N+1):
-    if i == X:
-        continue
-
-    result = dijkstra(i)
-
-    if result[X] + target_to_home[i] > max_time:
-        max_time = result[X] + target_to_home[i]
+    max_time = max(max_time,target_to_home[i] + home_to_target[i])
 
 print(max_time)
